6843d8b1891024f776030c1a6f24b127c29e74dd
julianascimentovieira <juh.scott18@gmail.com>
2024-02-14 20:47:43 -0300

    javascriptPt1

---
+++ quiz.js
@@ -0,0 +1,69 @@
+
+const quiz = [
+    {
+        pergunta: "A tag <head> é usada para definir o conteúdo principal de uma página.",
+        resposta: false
+    },
+    {
+        pergunta: "O seletor universal * seleciona todos os elementos em uma página.",
+        resposta: true
+
+    },
+
+    {
+        pergunta: "A função console.log() é usada para imprimir mensagens no console do navegador.",
+        resposta: true
+
+    },
+
+    {
+        pergunta: "A tag <main> é usada para representar o conteúdo principal de um documento HTML.",
+        resposta: false
+
+    },
+
+    {
+        pergunta: "A tag <article> é usada para representar um conteúdo independente que pode ser distribuído e reutilizado.",
+        resposta: true
+
+    },
+
+    {
+        pergunta: " A propriedade margin é usada para definir o espaçamento interno de um elemento. ",
+        resposta: false
+
+    },
+
+    {
+        pergunta: "A propriedade position: absolute; posiciona um elemento em relação ao seu contêiner pai.",
+        resposta: false
+
+    },
+
+    {
+        pergunta: "O método array.pop() remove o último elemento de um array.",
+        resposta: true
+
+    },
+
+    {
+        pergunta: "A tag <figure> é usada para incorporar conteúdo multimídia, como imagens, vídeos ou gráficos.",
+        resposta: false
+
+    },
+
+    {
+        pergunta: "A propriedade border-radius é usada para criar bordas arredondadas em elementos.",
+        resposta: true
+
+    },
+
+    {
+        pergunta: "A propriedade text-align é usada apenas para alinhar o texto horizontalmente, não afetando o alinhamento vertical.",
+        resposta: false
+
+    },
+    // Adicione mais perguntas conforme necessário
+];
+
+let perguntaAtual = 0;
